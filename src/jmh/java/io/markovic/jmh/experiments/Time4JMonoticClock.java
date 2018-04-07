e874e656c4fd1027e440f755114f078450fa5031
Val Markovic <vmarkovic@linkedin.com>
2018-04-06 17:11:32 -0700

    Updating tim4j benchmark

--- src/jmh/java/io/markovic/jmh/experiments/Time4JMonoticClock.java
+++ src/jmh/java/io/markovic/jmh/experiments/Time4JMonoticClock.java
@@ -29,36 +29,59 @@ public class Time4JMonoticClock {
   private Clock systemClock = Clock.systemUTC();
 
   @Benchmark
-  public Instant time4jClock() {
+  public Instant time4jClockWithInstant() {
     return time4JClock.instant();
   }
 
   @Benchmark
-  public Instant systemClock() {
+  public Instant systemClockWithInstant() {
     return systemClock.instant();
+  }
+
+  @Benchmark
+  public long time4jClockRawMillis() {
+    return time4JClock.millis();
+  }
+
+  @Benchmark
+  public long systemClockRawMillis() {
+    return systemClock.millis();
   }
 
   // GC RESULTS! (When run with `-prof gc`)
   //
-  // Benchmark                                                        Mode  Cnt    Score     Error   Units
-  // Time4JMonoticClock.systemClock                                   avgt   10   49.077 ±   9.356   ns/op
-  // Time4JMonoticClock.systemClock:·gc.alloc.rate                    avgt   10  314.980 ±  55.340  MB/sec
-  // Time4JMonoticClock.systemClock:·gc.alloc.rate.norm               avgt   10   24.000 ±   0.001    B/op
-  // Time4JMonoticClock.systemClock:·gc.churn.PS_Eden_Space           avgt   10  329.160 ±  97.821  MB/sec
-  // Time4JMonoticClock.systemClock:·gc.churn.PS_Eden_Space.norm      avgt   10   24.975 ±   4.580    B/op
-  // Time4JMonoticClock.systemClock:·gc.churn.PS_Survivor_Space       avgt   10    0.187 ±   0.116  MB/sec
-  // Time4JMonoticClock.systemClock:·gc.churn.PS_Survivor_Space.norm  avgt   10    0.014 ±   0.008    B/op
-  // Time4JMonoticClock.systemClock:·gc.count                         avgt   10   25.000            counts
-  // Time4JMonoticClock.systemClock:·gc.time                          avgt   10   38.000                ms
-  // Time4JMonoticClock.time4jClock                                   avgt   10   59.468 ±   9.524   ns/op
-  // Time4JMonoticClock.time4jClock:·gc.alloc.rate                    avgt   10  517.813 ±  75.589  MB/sec
-  // Time4JMonoticClock.time4jClock:·gc.alloc.rate.norm               avgt   10   48.000 ±   0.001    B/op
-  // Time4JMonoticClock.time4jClock:·gc.churn.PS_Eden_Space           avgt   10  538.022 ± 113.730  MB/sec
-  // Time4JMonoticClock.time4jClock:·gc.churn.PS_Eden_Space.norm      avgt   10   49.790 ±   6.463    B/op
-  // Time4JMonoticClock.time4jClock:·gc.churn.PS_Survivor_Space       avgt   10    0.210 ±   0.238  MB/sec
-  // Time4JMonoticClock.time4jClock:·gc.churn.PS_Survivor_Space.norm  avgt   10    0.019 ±   0.020    B/op
-  // Time4JMonoticClock.time4jClock:·gc.count                         avgt   10   49.000            counts
-  // Time4JMonoticClock.time4jClock:·gc.time                          avgt   10   67.000                ms
+  // Benchmark                                                                   Mode  Cnt    Score    Error   Units
+  // Time4JMonoticClock.systemClockRawMillis                                     avgt   10   34.354 ±  1.621   ns/op
+  // Time4JMonoticClock.systemClockRawMillis:·gc.alloc.rate                      avgt   10   ≈ 10⁻⁴           MB/sec
+  // Time4JMonoticClock.systemClockRawMillis:·gc.alloc.rate.norm                 avgt   10   ≈ 10⁻⁵             B/op
+  // Time4JMonoticClock.systemClockRawMillis:·gc.count                           avgt   10      ≈ 0           counts
+  // Time4JMonoticClock.systemClockWithInstant                                   avgt   10   45.255 ±  5.272   ns/op
+  // Time4JMonoticClock.systemClockWithInstant:·gc.alloc.rate                    avgt   10  338.761 ± 36.662  MB/sec
+  // Time4JMonoticClock.systemClockWithInstant:·gc.alloc.rate.norm               avgt   10   24.000 ±  0.001    B/op
+  // Time4JMonoticClock.systemClockWithInstant:·gc.churn.PS_Eden_Space           avgt   10  356.308 ± 87.294  MB/sec
+  // Time4JMonoticClock.systemClockWithInstant:·gc.churn.PS_Eden_Space.norm      avgt   10   25.110 ±  4.133    B/op
+  // Time4JMonoticClock.systemClockWithInstant:·gc.churn.PS_Survivor_Space       avgt   10    0.212 ±  0.071  MB/sec
+  // Time4JMonoticClock.systemClockWithInstant:·gc.churn.PS_Survivor_Space.norm  avgt   10    0.015 ±  0.006    B/op
+  // Time4JMonoticClock.systemClockWithInstant:·gc.count                         avgt   10   28.000           counts
+  // Time4JMonoticClock.systemClockWithInstant:·gc.time                          avgt   10   42.000               ms
+  // Time4JMonoticClock.time4jClockRawMillis                                     avgt   10   58.347 ±  8.978   ns/op
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.alloc.rate                      avgt   10  263.534 ± 34.303  MB/sec
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.alloc.rate.norm                 avgt   10   24.000 ±  0.001    B/op
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.churn.PS_Eden_Space             avgt   10  269.663 ± 75.456  MB/sec
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.churn.PS_Eden_Space.norm        avgt   10   24.608 ±  7.150    B/op
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.churn.PS_Survivor_Space         avgt   10    0.077 ±  0.136  MB/sec
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.churn.PS_Survivor_Space.norm    avgt   10    0.007 ±  0.014    B/op
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.count                           avgt   10   19.000           counts
+  // Time4JMonoticClock.time4jClockRawMillis:·gc.time                            avgt   10   30.000               ms
+  // Time4JMonoticClock.time4jClockWithInstant                                   avgt   10   59.870 ±  6.797   ns/op
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.alloc.rate                    avgt   10  512.263 ± 59.488  MB/sec
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.alloc.rate.norm               avgt   10   48.000 ±  0.001    B/op
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.churn.PS_Eden_Space           avgt   10  514.041 ± 86.099  MB/sec
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.churn.PS_Eden_Space.norm      avgt   10   48.180 ±  6.101    B/op
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.churn.PS_Survivor_Space       avgt   10    0.115 ±  0.112  MB/sec
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.churn.PS_Survivor_Space.norm  avgt   10    0.011 ±  0.011    B/op
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.count                         avgt   10   51.000           counts
+  // Time4JMonoticClock.time4jClockWithInstant:·gc.time                          avgt   10   72.000               ms
   //
   // We can see that speed-wise, the system clock and the time4j clock are
   // pretty much the same. Time4j is a bit slower, but not appreciably.
@@ -69,4 +92,6 @@ public class Time4JMonoticClock {
   // Time4j allocates 48 bytes per iteration while the system clock allocates
   // 24 bytes per iteration. That's twice as much, but probably not an amount
   // you'd care about.
+  // Interestingly, reading raw millis from the time4j clock still allocates
+  // (unlike doing the same with the system clock).
 }
